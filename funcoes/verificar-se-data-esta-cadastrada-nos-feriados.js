a375717ec935e30b1f1de639f656e7469008accf
Sergio Machado <sergiojfjfjf@hotmail.com>
2021-09-19 11:00:04 -0300

    Refatoração da função consultarFeriado

--- funcoes/verificar-se-data-esta-cadastrada-nos-feriados.js
+++ funcoes/verificar-se-data-esta-cadastrada-nos-feriados.js
@@ -1,25 +1,28 @@
 /**
  * Verifica se a data informada é um feriado
- * @param {String} dia Parâmetro obrigatório, dia do mês
- * @param {String} mes Parâmetro obrigatório, mês do ano, 01 para janeiro
- * @param {String} ano Parâmetro obrigatório, ano com 4 dígitos
+ * @param {String} data Parâmetro obrigatório, data no formato dd/MM/yyyy
  * @example 
- * consultarFeriado(25, 12, 2020) - Retorna true (natal)
- * @returns {boolean} Retorna true se a data exitir no cadastro de feriados
+ * consultarFeriado("25/12/2021") - Retorna "Natal"
+ * @returns {String} Nome do feriado caso ele exista
+ * @returns {boolean} false caso o feriado não exista
  * @author Sérgio Machado
  */
-function consultarFeriado(dia, mes, ano) {
+
+function consultarFeriado(data) {
+    var parseDate = new java.text.SimpleDateFormat("dd/MM/yyyy").parse(data).getTime();
+    var newDate = new Date(parseDate)
+
     var c0 = DatasetFactory.createConstraint("companyId", getValue("WKCompany"), getValue("WKCompany"), ConstraintType.MUST); // Código da Empresa
-    var c1 = DatasetFactory.createConstraint("holidayYear", ano, ano, ConstraintType.SHOULD); // Ano
+    var c1 = DatasetFactory.createConstraint("holidayYear", newDate.getFullYear(), newDate.getFullYear(), ConstraintType.SHOULD); // Ano
     var c2 = DatasetFactory.createConstraint("holidayYear", 0, 0, ConstraintType.SHOULD); // Ano Recorrente
-    var c3 = DatasetFactory.createConstraint("holidayMonth", mes, mes, ConstraintType.MUST) // Mês
-    var c4 = DatasetFactory.createConstraint("holidayDay", dia, dia, ConstraintType.MUST) // Dia
+    var c3 = DatasetFactory.createConstraint("holidayMonth", (newDate.getMonth() + 1), (newDate.getMonth() + 1), ConstraintType.MUST) // Mês
+    var c4 = DatasetFactory.createConstraint("holidayDay", newDate.getDate(), newDate.getDate(), ConstraintType.MUST) // Dia
 
     var constraints = new Array(c0, c1, c2, c3, c4);
     var dataset = DatasetFactory.getDataset("holiday", null, constraints, null);
 
     if (dataset.getRowsCount() > 0) {
-        return true;
+        return dataset.getValue(0, "holidayCalendarId");
     }
     return false
 }
